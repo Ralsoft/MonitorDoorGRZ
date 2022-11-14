97bff912dbf407938b1b4ff3a8eeee4de0e81e99
SmolerOK <sergey.develop@mail.ru>
2022-11-14 13:18:09 +0300

    сделана обработка потери и возобновления потери соединения

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import models.Messages;
 import models.Monitor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import service.JsonService;
 import service.MqttService;
 
@@ -11,54 +12,16 @@ public class Main {
 
     private static final Logger LOG = LogManager.getLogger(Main.class);
     static JsonService service = new JsonService();
-    static ObjectMapper mapper = new ObjectMapper();
+
     static MqttService mqttService = new MqttService();
     public static void main(String[] args) {
         LOG.info("Запуск программы.");
         try {
             mqttService.connectedMqtt(service.getConfigParam().getIpClient(), service.getConfigParam().getPortClient(), service.getConfigParam().getIdClient());
-
-            LOG.info("Попытка подписки на топик Parking/MonitorDoor/#");
-            mqttService.getMqttClient().subscribe("Parking/MonitorDoor/#", (topic, message) -> {
-                try {
-                    LOG.info("Получено сообщение. TOPIC: " + topic + " MESSAGE: " + message);
-                    String json = new String(message.getPayload());
-                    switch (topic) {
-                        case "Parking/MonitorDoor/Monitor/View" -> {
-                            try {
-                                LOG.info("Принят топик - Parking/MonitorDoor/Monitor/View");
-                                var monitor = mapper.readValue(json, Monitor.class);
-                                monitor.sendMessages();
-                                Thread.sleep(50);
-                            } catch (Exception ex) {
-                                LOG.error("Ошибка: " + ex);
-                            }
-                        }
-                        case "Parking/MonitorDoor/Door/Open" -> {
-                            try {
-                                LOG.info("Принят топик - Parking/MonitorDoor/Door/Open");
-                                var door = mapper.readValue(json, Door.class);
-                                door.openDoor0();
-                            } catch (Exception ex) {
-                                LOG.error("Ошибка: " + ex);
-                            }
-                        }
-                        case "Parking/MonitorDoor/Door/Warning/" -> {
-                            try {
-                                LOG.info("Принят топик - Parking/MonitorDoor/Door/Warning/");
-                                //
-                            } catch (Exception ex) {
-                                LOG.error("Ошибка: " + ex);
-                            }
-                        }
-                    }
-                } catch (Exception ex) {
-                    LOG.error("Ошибка: " + ex);
-                }
-            });
-            LOG.info("Подписка произошла успешно.");
         } catch (Exception ex) {
             LOG.error("Ошибка: " + ex);
         }
     }
+
+
 }
--- src/main/java/service/MqttService.java
+++ src/main/java/service/MqttService.java
@@ -1,11 +1,14 @@
 package service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import models.Door;
+import models.Monitor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.springframework.integration.mqtt.outbound.Mqttv5PahoMessageHandler;
+import org.eclipse.paho.client.mqttv3.*;
+
+import javax.sound.sampled.AudioFormat;
+import java.beans.Encoder;
 
 public class MqttService {
     private static final Logger LOG = LogManager.getLogger(MqttService.class);
@@ -14,6 +17,8 @@ public class MqttService {
     JsonService jsonService = new JsonService();
     MqttConnectOptions options;
 
+    static ObjectMapper mapper = new ObjectMapper();
+
     public void connectedMqtt(String host, int port, String clientName) throws InterruptedException {
         try {
             LOG.info("Попытка подключения. HOST: " + host + " PORT: " + port + " CLIENT_NAME: " + clientName);
@@ -21,24 +26,88 @@ public class MqttService {
                     "tcp://" + host
                             + ":" + port, MqttClient.generateClientId());
             options = new MqttConnectOptions();
-            options.setExecutorServiceTimeout(5000);
-            options.setMaxReconnectDelay(5000);
-            options.setKeepAliveInterval(5000);
             options.setAutomaticReconnect(true);
-            options.setConnectionTimeout(5000);
             options.setUserName(jsonService.getConfigParam().getMqttUsername());
             options.setPassword(jsonService.getConfigParam().getMqttPassword().toCharArray());
+
+
+            mqttClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable throwable) {
+                    LOG.error("Соединение потеряно " + throwable.getMessage());
+                    if (!mqttClient.isConnected()) {
+                        try {
+                            Thread.sleep(5000);
+                            connectedMqtt(host, port, clientName);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+
+                @Override
+                public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                    LOG.info("пришло сообщение " + s);
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                    LOG.info("deliveryComplete " + iMqttDeliveryToken.toString());
+                }
+            });
             mqttClient.connect(options);
+            subscribe();
             LOG.info("Успешное подключение.");
         } catch (MqttException e) {
-            Thread.sleep(5000);
             LOG.error("Ошибка: " + e);
             if (!mqttClient.isConnected()) {
+                Thread.sleep(5000);
                 connectedMqtt(host, port, clientName);
             }
         }
     }
 
+    private void subscribe() throws MqttException {
+        LOG.info("Попытка подписки на топик Parking/MonitorDoor/#");
+        mqttClient.subscribe("Parking/MonitorDoor/#", (topic, message) -> {
+            try {
+                LOG.info("Получено сообщение. TOPIC: " + topic + " MESSAGE: " + message);
+                String json = new String(message.getPayload());
+                switch (topic) {
+                    case "Parking/MonitorDoor/Monitor/View" -> {
+                        try {
+                            LOG.info("Принят топик - Parking/MonitorDoor/Monitor/View");
+                            var monitor = mapper.readValue(json, Monitor.class);
+                            monitor.sendMessages();
+                            Thread.sleep(50);
+                        } catch (Exception ex) {
+                            LOG.error("Ошибка: " + ex);
+                        }
+                    }
+                    case "Parking/MonitorDoor/Door/Open" -> {
+                        try {
+                            LOG.info("Принят топик - Parking/MonitorDoor/Door/Open");
+                            var door = mapper.readValue(json, Door.class);
+                            door.openDoor0();
+                        } catch (Exception ex) {
+                            LOG.error("Ошибка: " + ex);
+                        }
+                    }
+                    case "Parking/MonitorDoor/Door/Warning/" -> {
+                        try {
+                            LOG.info("Принят топик - Parking/MonitorDoor/Door/Warning/");
+                            //
+                        } catch (Exception ex) {
+                            LOG.error("Ошибка: " + ex);
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                LOG.error("Ошибка: " + ex);
+            }
+        });
+        LOG.info("Подписка произошла успешно.");
+    }
     public MqttClient getMqttClient() {
         return mqttClient;
     }
